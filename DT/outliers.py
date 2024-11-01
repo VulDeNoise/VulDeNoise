4563407654e7107da9e801c2372d9911df8d6709
VulDeNoise <132751760+VulDeNoise@users.noreply.github.com>
2024-11-01 20:55:44 +0800

    Update outliers.py

--- DT/outliers.py
+++ DT/outliers.py
@@ -5,7 +5,21 @@ import json
 import hashlib
 from turtle import position
 from sklearn.ensemble import IsolationForest
-from sklearn.cluster import MiniBatchKMeans
+#from sklearn.cluster import MiniBatchKMeans
+# from pyod.models.abod import ABOD
+# from pyod.models.auto_encoder import AutoEncoder
+# from pyod.models.cblof import CBLOF
+# from pyod.models.hbos import HBOS
+# from pyod.models.iforest import IForest
+# from pyod.models.knn import KNN
+# from pyod.models.lof import LOF
+# from pyod.models.mcd import MCD
+# from pyod.models.so_gaal import SO_GAAL
+# from pyod.models.ocsvm import OCSVM
+# from pyod.models.sos import SOS
+# from pyod.models.pca import PCA
+# from pyod.models.ecod import ECOD
+
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
@@ -20,12 +34,13 @@ import csv
 
 def main():
     res_loss={}
-    with open('###/AST_loss.json','r') as af:
+    with open('/home/DT/AST_loss.json','r') as af:
         AST_res=json.load(af)
-    with open('###/CFG_loss.json','r') as cf:
+    with open('/home/DT/CFG_loss.json','r') as cf:
         CFG_res=json.load(cf)
-    with open('####/PDG_loss.json','r') as pf:
+    with open('/home/DT/PDG_loss.json','r') as pf:
         PDG_res=json.load(pf)
+    # for item in AST_res:
     for item in PDG_res:
         try:
             # matrix1 = np.matrix(AST_res[item])
@@ -33,28 +48,33 @@ def main():
             # matrix3 = np.matrix(PDG_res[item])
             # matrix4 = np.concatenate((matrix1,matrix2,matrix3), axis=1)
             matrix4 = AST_res[item]+CFG_res[item]+PDG_res[item]
-            # matrix4 = PDG_res[item]+CFG_res[item]
+            # matrix4 = CFG_res[item] + PDG_res[item]
             res_loss[item] = matrix4
         except:
             continue
     print('before filter len:\t%d'%(len(res_loss)))
+    # with open('/home/DT/dataset/no_noise.txt','a') as w1p:
+    #     for item in list(res_loss.keys()):
+    #         item="/home/DT_devign/devign_dataset/func_embedding/PDG_input/"+item
+    #         w1p.write(item+'\n')
+    # w1p.close()
     for outliers_fraction in np.arange(0.05, 0.35, 0.05): # 0.05 - 0.5
         print('====> outliers_fraction:\t%.3f'%(outliers_fraction))
         final_keep_slice_path, iforest = iforest_filter(res_loss, outliers_fraction)
         print(len(final_keep_slice_path))
         revise_label = list(set(list(res_loss.keys())) - set(final_keep_slice_path))
-        with open('#####/revise_%.2f.txt'%(outliers_fraction),'a') as w1p:
-           for item in final_keep_slice_path:
-               item="####/PDG_input/"+item
-               w1p.write(item+'\n')
-        w1p.close()
-        with open('####/revise_%.2f.txt'%(outliers_fraction),'a') as w1p:
+        # with open('/home/DT/dataset/revise_%.2f.txt'%(outliers_fraction),'a') as w1p:
+        #    for item in final_keep_slice_path:
+        #        item="/home/Dataset/fire/6_vec/pdg/"+item
+        #        w1p.write(item+'\n')
+        # w1p.close()
+        with open('/home/DT/dataset/revise_%.2f.txt'%(outliers_fraction),'a') as w1p:
            for item in revise_label:
                if int(item[0]) == 0:
-                   item="####/PDG_input/"+'1\'_CVE'+item.split('CVE')[-1]
+                   item="/home/Dataset/6_vec/pdg/"+'1\'_'+item[2:]
                else:
-                   item="######/PDG_input/"+'0\'_CVE'+item.split('CVE')[-1]
-               w1p.write(item+'\n')
+                   item=item="/home/Dataset/6_vec/pdg/"+'0\'_'+item[2:]
+                    w1p.write(item+'\n')
         w1p.close()
 
 def iforest_filter(keep_slice_dict, outliers_fraction):
@@ -63,7 +83,7 @@ def iforest_filter(keep_slice_dict, outliers_fraction):
     for func_ in keep_slice_dict.keys():
         vul_keep_slices.append(keep_slice_dict[func_])
         slice_name_list.append(func_)
-    rng = np.random.RandomState(42)
+    rng = np.random.RandomState(46)
     clf = IsolationForest(max_samples=len(vul_keep_slices), random_state=rng, contamination=outliers_fraction)
     clf.fit(X=vul_keep_slices)
 
@@ -75,6 +95,7 @@ def iforest_filter(keep_slice_dict, outliers_fraction):
         if _score > threshold:
             final_keep_slice_path.append(slice_name_list[i])
     return final_keep_slice_path, clf
+
 
 if __name__ == '__main__':
     main()
